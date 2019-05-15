341babd9e96d5f7c68fdbe25d1b7d0814cd61078
Jinji Wei <jinjiwei@Jinjis-MacBook-Pro.local>
2019-05-15 11:37:55 +0800

    接口-可选属性

--- examples/section3/index.ts
+++ examples/section3/index.ts
@@ -1,3 +1,4 @@
+/* 接口初探 */
 interface LabelledValue {
     label: string
 }
@@ -7,3 +8,25 @@ function printLabel(labelledObj: LabelledValue) {
 }
 let myObj = { size: 100, label: 'hhh' }
 printLabel(myObj)
+
+
+/* 可选属性 */
+interface Square {
+    color: string
+    area: number
+}
+interface SquareConfig {
+    color?: string
+    width?: number
+}
+function createSquare(config: SquareConfig): Square {
+    let newSquare = { color: '#ffffff', area: 100 }
+    if (config.color) {
+        newSquare.color = config.color
+    }
+    if (config.width) {
+        newSquare.area = config.width * config.width
+    }
+    return newSquare
+}
+createSquare({ color: '#000000' })
