cbb6fbed5389462912c7f3a96c57f3031687a1b7
Jinji Wei <jinjiwei@Jinjis-MacBook-Pro.local>
2019-05-15 11:50:38 +0800

    接口-只读属性

--- examples/section3/index.ts
+++ examples/section3/index.ts
@@ -30,3 +30,21 @@ function createSquare(config: SquareConfig): Square {
     return newSquare
 }
 createSquare({ color: '#000000' })
+
+
+/* 只读属性 */
+interface Point {
+    readonly x:number
+    readonly y:number
+}
+let p1:Point = { x: 10, y: 90 } // 只能在定义的时候给只读属性赋值，之后不可在修改
+// p1.x = 99  这样会报错
+
+/* 范型只读数组 */
+let o1: number[] = [1,2,3,4]
+let ro: ReadonlyArray<number> = o1 // 此时就不能对ro进行修改
+// ro[0] = 12
+// ro.length = 90
+// o1 = ro
+o1 = ro as number[] // 此时用类型断言是不会报错的
+// 只读属性和const  如果是变量的话用const 如果是属性的话用只读属性
