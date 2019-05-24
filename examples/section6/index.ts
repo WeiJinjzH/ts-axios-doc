878ff10a204f8ce77960ed8d44e606a73eda1fa9
Jinji Wei <jinjiwei@Jinjis-MacBook-Pro.local>
2019-05-24 14:44:04 +0800

    泛型-泛型变量的使用

--- examples/section6/index.ts
+++ examples/section6/index.ts
@@ -3,8 +3,17 @@
 
 
 /* 基本示例 */
-function identify<T>(arg): T { // 它不同于any，当传入number时返回的也是number
+function identity<T>(arg): T { // 它不同于any，当传入number时返回的也是number
     return arg
 }
-let output = identify<string>('myString') // 准确类型
-let output1 = identify('myString') // 类型推断
+let output = identity<string>('myString') // 准确类型
+let output1 = identity('myString') // 类型推断
+
+
+
+
+/* 使用泛型变量 */
+function loggingIdentity<T>(arg: T[]): T[] {
+    console.log(arg.length)
+    return arg
+}
