7d04812e3c959f6babebff9ab12245f1121a5fd5
WeiJinjzH <991775996@qq.com>
2019-07-08 17:49:21 +0800

    feat(demo编写): 处理请求body数据-demo编写
    demo编写
    
    fix #123

--- src/helpers/data.ts
+++ src/helpers/data.ts
@@ -2,6 +2,6 @@ import { isPlainObject } from './util'
 
 export function transformRequest(data: any): any {
   if (isPlainObject(data)) {
-    return data.stringify(data)
+    return JSON.stringify(data)
   }
 }
