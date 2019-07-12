56f5efe52b021ac5ebc0ddbe970f839e449156b6
WeiJinjzH <991775996@qq.com>
2019-07-12 11:30:15 +0800

    feat(all file): header处理逻辑
    处理请求header-实现请求header处理逻辑
    
    fix #123

--- src/xhr.ts
+++ src/xhr.ts
@@ -1,10 +1,17 @@
 import { AxiosRequestConfig } from './type'
 
 export default function xhr(config: AxiosRequestConfig): void {
-  const { data = null, method = 'get', url } = config
+  const { data = null, method = 'get', url, headers } = config
 
   const request = new XMLHttpRequest()
 
   request.open(method.toUpperCase(), url, true) // 默认异步，设置为true,method要转为大写
   request.send(data)
+  Object.keys(headers).forEach(name => {
+    if (data === null && name.toLowerCase() === 'content-type') {
+      delete headers[name]
+    } else {
+      request.setRequestHeader(name, headers[name])
+    }
+  })
 }
